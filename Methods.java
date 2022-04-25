42db7ae9494c1f3380944f15a2afce658934d1d6
HarryRitchie98 <harry.ritchie@atos.net>
2022-04-25 17:41:57 +0100

    Main menu options

--- Methods.java
+++ Methods.java
@@ -6,22 +6,19 @@ import java.util.Scanner;
 
 public class Methods {
 
-    //Declarations
-    int user_id_entry;
+    public void MainMenuSelect() {
 
-    //SQL operations
-    public String insertUsersSQL = "INSERT INTO users (username, password, email, name)" + " VALUES (?, ?, ?, ?)";
-    public String selectAllUsersSQL = "SELECT * FROM users";
-    public String updateUsersSQL = "UPDATE users SET username = ?, password = ?, email = ?, name = ? WHERE user_id = ?";
-    public String deleteUsersSQL = "DELETE FROM users WHERE user_id =?";
+        //Main menu options
+        System.out.println("Select from the options below:");
+        System.out.println("Press 1 to add a customer to the system \n Press 2 to view all customers in the system \n " +
+                "Press 3 to update a customer in the system \n Press 4 to delete a customer in the system");
+        Scanner menuSelect = new Scanner(System.in);
+        String dataInput = menuSelect.nextLine();
 
-    //SQL DB credentials
-    public String jdbcURL = "jdbc:mysql://localhost:3306/sampledb";
-    public String usernameDB = "root";
-    public String passwordDB = "password";
+    }
+}
 
 
-        }
 
 
 
